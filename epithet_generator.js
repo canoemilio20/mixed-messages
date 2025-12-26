576787f376e71b722156afbb52dd9df5b8a2a523
Emilio <canoemilio20@gmail.com>
2025-12-26 12:33:02 -0600

    Generate the epithet

--- epithet_generator.js
+++ epithet_generator.js
@@ -62,3 +62,41 @@ const phrases = [
     'Answer to No One',
     'Marked by the Long Dark'
 ];
+
+const epithetGenerator = () => {
+
+    const type = Math.floor(Math.random() * 4) + 1; // generate number from 1 to 4 to identify the type of epithet to generate
+
+    if(type == 1){ // type 1: classic epic epithet (adjective + methaphor)
+
+        const adjective = Math.floor(Math.random() * adjectives.length); // pick the index of the adjective (from 0 to the max index, due to the generated number being floored)
+        const metaphor = Math.floor(Math.random() * metaphoresAndAllusions.length); // pick the index of the metaphor (from 0 to the max index, due to the generated number being floored)
+
+        return `${adjectives[adjective]} ${metaphoresAndAllusions[metaphor]}`;
+
+    } else if(type == 2){ // type 2: mythic title epithet (adjective + phrase)
+
+        const adjective = Math.floor(Math.random() * adjectives.length); // pick the index of the adjective (from 0 to the max index, due to the generated number being floored)
+        const phrase = Math.floor(Math.random() * phrases.length); // pick the index of the phrase (from 0 to the max index, due to the generated number being floored)
+
+        return `${adjectives[adjective]}, ${phrases[phrase]}`;
+
+    } else if(type == 3){ // type 3: grand epithet (adjective + metaphor, phrase)
+
+        const adjective = Math.floor(Math.random() * adjectives.length); // pick the index of the adjective (from 0 to the max index, due to the generated number being floored)
+        const metaphor = Math.floor(Math.random() * metaphoresAndAllusions.length); // pick the index of the metaphor (from 0 to the max index, due to the generated number being floored)
+        const phrase = Math.floor(Math.random() * phrases.length); // pick the index of the phrase (from 0 to the max index, due to the generated number being floored)
+
+        return `${adjectives[adjective]} ${metaphoresAndAllusions[metaphor]}, ${phrases[phrase]}`;
+
+    } else if(type == 4){ // type 4: short and ominous epithet (adjective)
+
+        const adjective = Math.floor(Math.random() * adjectives.length); // pick the index of the adjective (from 0 to the max index, due to the generated number being floored)
+
+        return `${adjectives[adjective]}`;
+
+    }
+
+}
+
+console.log(epithetGenerator());
